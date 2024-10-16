13ebe0e98d3cec5d1a694abe9c016289dd749f69
Ikwu Idoko Francis <58372655+cisco00@users.noreply.github.com>
2024-10-16 11:21:50 +0100

    updated database

--- data_ELT_process.py
+++ data_ELT_process.py
@@ -1,6 +1,21 @@
 import yfinance as stock_data
+from pymongo import MongoClient
+from sqlalchemy.orm.collections import collection
 
 ticker = "AAPL"
 
 stock_dt = stock_data.download(ticker, start = "2024-01-01", end="2024-09-01")
 
+# Converting data to a json file for easy access by mongo
+stock_dt = stock_dt.to_dict(orient='records')
+
+#Coonecting to Mongodb
+client = MongoClient('localhost', 27017)
+
+# Connecting to database
+db = client['financeStockData']
+
+collections = db['stock_data']
+
+# Populating the table
+collections.insert_many(stock_dt)
