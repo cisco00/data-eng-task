0806569acc2a6eacabc1c1d82ca68b64254eb0d6
cisco00 <idokofrancis66@gmail.com>
2024-10-17 15:58:08 +0100

    created notebook file for forecasting model

--- data_ELT_process.py
+++ data_ELT_process.py
@@ -44,36 +44,3 @@ class DataELT:
 if __name__ == "__main__":
     elt = DataELT()
     elt.update()
-
-def run_data_update():
-    data = DataELT()
-    data.update()
-
-# Creating an automation with airflow
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-
-default_args = {
-    "owner": "Ikwu_Francis",
-    "depends_on_past": False,
-    "start_date": datetime(2020, 1, 1),
-    "email": ["idokofrancis66@gmail.com"],
-    "email_on_failure": True,
-    "email_on_retry": True,
-    "retries": 3,
-    "retry_delay": timedelta(minutes=3),
-}
-
-with DAG(
-    'daily_stock_data_update',
-    default_args=default_args,
-    description='DAG to update stock data daily using PythonOperator',
-    schedule='0 0 * * *',  # Run every day at midnight
-    catchup=False,
-) as dag:
-
-    update_stock_data = PythonOperator(
-        task_id='run_dag',
-        python_callable=run_data_update,
-    )
-update_stock_data
